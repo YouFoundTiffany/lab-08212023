1376a8ba66178ce442e2dcd41eaf1ecd25f49b12
tif <youfoundtiffany@gmail.com>
2023-08-28 13:52:51 -0600

    update commits

--- app/controllers/PlayersController.js
+++ app/controllers/PlayersController.js
@@ -8,20 +8,24 @@ export class PlayersController {
         console.log('player controller loaded')
         this.drawPlayers()
     }
-    drawPlayers(){
-    // needs the players,
-    // generate content (html) to display
-    // inject html to page
-let content =''
-AppState.players.forEach (player => {
-//     // div template
-//     content += `<div class="col-12 p-3 bg-light rounded elevetion-2">
-//     ${this.name} <span>mdi-plus</span>${this.points} <span>mdi-minus</span>
-//   </div>`
-  content += player.cardTemplate
-})
-document.getElementById('players').innerHTML = content
-
-
+    drawPlayers() {
+        // needs the players,
+        // generate content (html) to display
+        // inject html to page
+        let content = ''
+        AppState.players.forEach(player => {
+            //     // div template
+            //     content += `<div class="col-12 p-3 bg-light rounded elevetion-2">
+            //     ${this.name} <span onclick="app.PlayerController.scorePoint" >mdi-plus</span>${this.points} <span>mdi-minus</span>
+            //   </div>`
+            content += player.cardTemplate
+        })
+        const elem = document.getElementById('players')
+        if (elem) {
+            elem.innerHTML = content
+        }
+    }
+    scorePoint(playerName) {
+        console.log('clicked', playerName)
     }
 }
--- app/models/Player.js
+++ app/models/Player.js
@@ -1,22 +1,22 @@
 export class Player {
-name
-score
-image
-constructor (newName, newPoints){
+  name
+  score
+  image
+  constructor(newName, newPoints, newImage) {
     this.name = newName
     this.score = newPoints
     this.image = newImage
 
-}
-greeting() {
-  console.log('hello my name is', this.name)
-}
+  }
+  greeting() {
+    console.log('hello my name is', this.name)
+  }
 
 
 
-get cardTemplate () {
-    return `<div class="col-8 bg-light p-2 rounded elevetion-2"><img class="profPic" src="${this.name}">${this.name} <span>mdi-plus</span>${this.score} <span>mdi-minus</span>
+  get cardTemplate() {
+    return `<div class="col-8 p-2 bg-light p-2 rounded elevetion-2"><img class="profPic" src='${this.image}'>${this.name} <span class></span>${this.score} <span></span>
   </div>
   <div class="p-2"></div>`
-}
+  }
 }
